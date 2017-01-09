00bc7f7ae723f8f5ce5cc3114e098d652ab011bb
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2017-01-09 12:01:50 +0100

    Build Nuget package und nuspec updated.

---
+++ Ifp.Validation.WPF/BuildNugetPackage.bat
@@ -0,0 +1,3 @@
+"%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" Ifp.Validation.WPF.csproj /t:Build /p:Configuration="Release 4.6"
+"%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" Ifp.Validation.WPF.csproj /t:Build /p:Configuration="Release 4.5"
+"%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" Ifp.Validation.WPF.csproj /t:Build;Package /p:Configuration="Release 4.0"
